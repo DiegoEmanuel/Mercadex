574a2e71f55c1570aed7ae9195a73a05d8d3611f
Diego EFC <43099313+DiegoEmanuel@users.noreply.github.com>
2025-02-02 18:17:48 -0300

    Implement CRUD methods for ProductService in frontend

--- mercado_nxt/app/services/productService.ts
+++ mercado_nxt/app/services/productService.ts
@@ -3,7 +3,11 @@ import { Product } from '../types/product';
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export class ProductService {
+  //todo melhoria criar uma classe pra tratar erros
+  //todo melhoria criar classe pra montar as requisições
+  //get
   static async getProducts(): Promise<Product[]> {
+
     try {
       const response = await fetch(`${API_URL}/products`, {
         next: { revalidate: 0 }
@@ -31,5 +35,65 @@ export class ProductService {
       console.error('Erro ao deletar produto:', error);
       throw error;
     }
+  }
+  //create
+  static async createProduct(product: Product): Promise<Product> {
+    try {
+      const response = await fetch(`${API_URL}/products`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(product)
+      });
+
+      if (!response.ok) {
+        throw new Error('Falha ao criar produto');
+      }
+
+      return response.json();
+    } catch (error) {
+      console.error('Erro ao criar produto:', error);
+      throw error;
+    }
+  }
+
+  //update
+  static async updateProduct(id: number, product: Product): Promise<Product> {
+    try {
+      const response = await fetch(`${API_URL}/products/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(product)
+      });
+
+      if (!response.ok) {
+        throw new Error('Falha ao atualizar produto');
+      } 
+
+      return response.json();
+    } catch (error) {
+      console.error('Erro ao atualizar produto:', error);
+      throw error;
+    }
+  }
+
+  //getById
+  static async getProductById(id: number): Promise<Product> {
+    try {
+      const response = await fetch(`${API_URL}/products/${id}`);
+
+      if (!response.ok) {
+        throw new Error('Falha ao buscar produto por ID');
+      }
+
+      return response.json();
+    }
+      catch (error) {
+        console.error('Erro ao buscar produto por ID:', error);
+        throw error;
+      }
   }
 } 
